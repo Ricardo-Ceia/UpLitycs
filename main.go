1b8db6105afde60aaf82626bb226a600cb2cb18d
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-09-16 16:53:51 +0100

    testing routing patterns and url parameters

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+func getUserName(w http.ResponseWriter, r * http.Request){
+	userName := chi.URLParam(r,"userName")
+
+	w.Write([]byte("Hello "+userName))
+}
+
 func main(){
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -14,6 +20,8 @@ func main(){
 	r.Get("/",func(w http.ResponseWriter, r *http.Request){
 		w.Write([]byte("root."))
 	})
+	
 
+	r.Get("/{userName}",getUserName)
 	http.ListenAndServe(":3333",r)
 }
