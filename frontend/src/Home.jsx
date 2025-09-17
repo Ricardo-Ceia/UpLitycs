2dfb9d5644b5aa226ca02ebc6df1f1faa9c35108
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-09-17 17:45:30 +0100

    adding a slight animation to the HomePage CTA title

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,9 @@
-function Home() {
+import "./App.css";
 
-return (
+function Home() {
+  return (
     <section className="bg-background min-h-screen flex flex-col justify-center items-center px-6 text-center space-y-6">
-      <h1 className="text-4xl md:text-5xl font-bold text-primary leading-tight">
-        Monitor Your Services. Catch Issues Before They Catch You.
-      </h1>
+      <ShinyText text="Monitor Your Services. Catch Issues Before They Catch You." />
       <p className="text-textSecondary text-lg md:text-xl max-w-xl">
         Uplytics keeps your websites, APIs, and apps running smoothly with real-time alerts and a public status page.
       </p>
@@ -12,9 +11,24 @@ return (
         Get Started - Free
       </button>
     </section>
-  )
+  );
 }
 
+function ShinyText({ text }) {
+  return (
+    <h1 className="text-4xl md:text-5xl font-bold text-primary leading-tight">
+      {text.split("").map((c, index) => (
+        <span
+          key={index}
+          style={{ animationDelay: `${index * 0.1}s` }}
+          className="shiny-letter"
+        >
+          {c}
+        </span>
+      ))}
+    </h1>
+  );
+}
 
+export default Home;
 
-export default  Home
