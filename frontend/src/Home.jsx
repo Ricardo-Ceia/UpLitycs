f561be10e2496e63042e5a4fad5df128bc44345c
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-09-18 13:02:50 +0100

    add a semaphore skeuomorphic element to homePage

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,19 +1,35 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function Home() {
   return (
-    <section className="bg-background min-h-screen flex flex-col justify-center items-center px-6 text-center space-y-6">
-      <ShinyText text="Monitor Your Services. Catch Issues Before They Catch You." />
-      <p className="text-textSecondary text-lg md:text-xl max-w-xl">
-        Uplytics keeps your websites, APIs, and apps running smoothly with real-time alerts and a public status page.
-      </p>
-      <button className="retro-button">
-        Get Started - Free
-      </button>
+    <section className="relative min-h-screen flex flex-col justify-center items-center px-6 text-center space-y-6 overflow-hidden">
+        <StatusLight/>
+        <ShinyText text="Monitor Your Services. Catch Issues Before They Catch You." />
+        <p className="text-textSecondary text-lg md:text-xl max-w-xl mt-4">
+          Uplytics keeps your websites, APIs, and apps running smoothly with real-time alerts and a public status page.
+        </p>
+        <button className="retro-button mt-6">
+          Get Started - Free
+        </button>
     </section>
   );
 }
 
+function StatusLight(){
+  const [active, setActive] = useState("green");
+
+  return (
+      <div className="status-lights">
+        {["red", "yellow", "green"].map(color => (
+          <div
+            key={color}
+            className={`led ${color} ${active === color ? "active" : ""}`}
+            onClick={() => setActive(color)}
+          />
+        ))}
+      </div>
+  )
+}
 function ShinyText({ text }) {
   return (
     <h1 className="text-4xl md:text-5xl font-bold text-primary leading-tight">
