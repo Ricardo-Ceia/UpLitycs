c40c5a7de1330df8c0a256356ff15ce65e10aa2e
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-09-24 20:28:32 +0100

    getting frontend routing logic to work on the backend

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ echo "Starting database container..."
 cd .. && docker compose -f docker-compose.yaml up -d db
 
 echo "Building frontend..."
-cd frontend && npm run build
+cd frontend && npm install && npm run build
 
-echo "Building backend..."
-cd ..  && go run .
+echo "Starting backend..."
+cd .. && go run .
